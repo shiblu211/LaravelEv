bbfcbfe814cd0e1f1e5a72f094f27ba8da87a7f2
Emdadul Haque <shiblu.mail@gmail.com>
2022-04-25 15:09:12 +0600

    Laravel UI init, Migration and model created

---
+++ database/migrations/2022_04_25_073443_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->unsignedInteger('subcategory_id');
+            $table->float('price');
+            $table->string('thumbnail');
+            $table->timestamps();
+
+            $table->foreign('subcategory_id')
+                ->references('id')->on('subcategories')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
