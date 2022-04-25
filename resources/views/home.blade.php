2f9c78e4ef5d35365367824e187c24540dd666ff
Emdadul Haque <shiblu.mail@gmail.com>
2022-04-25 23:48:19 +0600

    final commit for today

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="d-flex container" id="wrapper">
-@include('layouts.sidebar')
+    <div class="d-flex container" id="wrapper">
+    @include('layouts.sidebar')
     <!-- Page Content -->
-    <div id="page-content-wrapper">
-        <div class="container-fluid">
-            <h1 class="mt-4">Hi</h1>
-            <p>Hellow</p>
+        <div id="page-content-wrapper">
+            <div class="container-fluid">
+                <h1 class="mt-4">Hi</h1>
+                <p>Would appreciate if you check My Coding. I started from today, or else I could do better. If you
+                    Allow I will update my code. <br>
+                So far All codes are functional, Thanks for Checking
+                </p>
+            </div>
         </div>
+        <!-- /#page-content-wrapper -->
     </div>
-    <!-- /#page-content-wrapper -->
-</div>
-<!-- /#wrapper -->
+    <!-- /#wrapper -->
 @endsection
